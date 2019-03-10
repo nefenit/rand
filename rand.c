9ed2cd6b896a82a7636462a1dbe93e9fa67deac3
nefenit <nefenit@gmail.com>
2019-03-10 21:26:03 +0100

    Added head comment

--- rand.c
+++ rand.c
@@ -1,3 +1,7 @@
+/* rand - generates random integrer within range using rand()
+ * (c) Copyright 2019 Bartosz Mierzynski
+ * Written in ANSI C (C89)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
