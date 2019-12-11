9567ca96fb265cde714c1b3d83852a625943d74f
nefenit <nefenit@gmail.com>
2019-12-11 16:38:38 +0100

    added usage and and some doxygen comments

--- rand.c
+++ rand.c
@@ -1,22 +1,27 @@
-/* rand - generates random integrer within range using rand()
- * (c) Copyright 2019 Bartosz Mierzynski
- * Written in ANSI C (C89)
+/**
+ * @file    rand.c
+ * @author  Bartosz Mierzynski <nefenit@gmail.com>
+ * @date    2019
+ * @version 1.0
+ * @brief   Generates pseudorandom integer using rand().
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-int random_int(int mn, int mx) {
-	return mn + rand() / (RAND_MAX / (mx - mn + 1) + 1);
+int random_int(int min, int max) {
+	return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	
-	switch(argc) {
+	switch(argc) {	
 	case 0:
 	case 1:
+		printf("%d\n", rand());
 		break;
 	case 2:
 		printf("%d\n", rand() % (atoi(argv[1]) + 1));
@@ -25,7 +30,8 @@ int main(int argc, char *argv[]) {
 		printf("%d\n", random_int(atoi(argv[1]), atoi(argv[2])));
 		break;
 	default:
-		break;
+		fprintf(stderr, "usage: rand [MAX]\n       rand MIN MAX\n");
 	}
-	return 0;
+	
+	return EXIT_SUCCESS;
 }
